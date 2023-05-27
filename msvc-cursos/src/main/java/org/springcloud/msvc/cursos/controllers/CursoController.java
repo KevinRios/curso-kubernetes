3c22842b4a69f1a6f466939061c87084013c010e
Kevin Rios <kevinjirios@gmail.com>
2023-05-27 19:21:50 -0300

    add validaciones para cursos

--- msvc-cursos/src/main/java/org/springcloud/msvc/cursos/controllers/CursoController.java
+++ msvc-cursos/src/main/java/org/springcloud/msvc/cursos/controllers/CursoController.java
@@ -5,9 +5,13 @@ import org.springcloud.msvc.cursos.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -32,13 +36,19 @@ public class CursoController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<?> crearCurso(@RequestBody Curso curso){
+    public ResponseEntity<?> crearCurso(@Valid @RequestBody Curso curso, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return validar(bindingResult);
+        }
         Curso cursoGuardado = cursoService.guardarCurso(curso);
         return ResponseEntity.status(HttpStatus.CREATED).body(cursoGuardado);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> editarCurso(@RequestBody Curso curso, @PathVariable Long id){
+    public ResponseEntity<?> editarCurso(@Valid @RequestBody Curso curso, BindingResult bindingResult, @PathVariable Long id){
+        if (bindingResult.hasErrors()){
+            return validar(bindingResult);
+        }
         Optional<Curso> cursoOptional = cursoService.buscarCursoPorId(id);
         if (cursoOptional.isPresent()){
             Curso cursoEdit = cursoOptional.get();
@@ -56,6 +66,14 @@ public class CursoController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    private ResponseEntity<Map<String, String>> validar(BindingResult bindingResult) {
+        Map<String, String> errores = new HashMap<>();
+        bindingResult.getFieldErrors().forEach(error ->{
+            errores.put(error.getField(), "Campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errores);
     }
 
 

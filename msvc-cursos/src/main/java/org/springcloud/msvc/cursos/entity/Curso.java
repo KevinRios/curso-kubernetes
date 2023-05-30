006c1297ba3cbcefd49cc138b3d48748119f4413
Kevin Rios <kevinjirios@gmail.com>
2023-05-29 21:10:48 -0300

    add relacion entre curso y usuarios

--- msvc-cursos/src/main/java/org/springcloud/msvc/cursos/entity/Curso.java
+++ msvc-cursos/src/main/java/org/springcloud/msvc/cursos/entity/Curso.java
@@ -1,7 +1,11 @@
 package org.springcloud.msvc.cursos.entity;
 
+import org.springcloud.msvc.cursos.models.Usuario;
+
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "cursos")
@@ -11,9 +15,22 @@ public class Curso {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @NotEmpty
     private String nombre;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "curso_id")
+    private List<CursoUsario> cursoUsarios;
+
+    @Transient
+    private List<Usuario> usuarios;
+
+
+
+    public Curso() {
+        cursoUsarios = new ArrayList<>();
+        usuarios = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -29,5 +46,29 @@ public class Curso {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public List<CursoUsario> getCursoUsarios() {
+        return cursoUsarios;
+    }
+
+    public void setCursoUsarios(List<CursoUsario> cursoUsarios) {
+        this.cursoUsarios = cursoUsarios;
+    }
+
+    public void addCursoUsuario(CursoUsario cursoUsario){
+        cursoUsarios.add(cursoUsario);
+    }
+
+    public void removeCursoUsuario(CursoUsario cursoUsario){
+        cursoUsarios.remove(cursoUsario);
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
     }
 }
